40abf9ce8a35fc6a5d9cc73a5ec0fdae195fc9dd
dozieaji <shedrack@zoracom.com>
2019-09-29 12:46:12 -0700

    First commit

---
+++ src/main/java/rc/bootsecurity/BootSecurityApplication.java
@@ -0,0 +1,16 @@
+package rc.bootsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+
+@SpringBootApplication
+public class BootSecurityApplication extends SpringBootServletInitializer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootSecurityApplication.class, args);
+    }
+}
+
+
